0bf58173543c1770c10cae882d3ce77f83f08545
2017mrhi <2017mrhi@gmail.com>
2023-11-20 15:36:14 +0900

    modify VO object oracle driver JAR library

--- src/main/java/member/model/MemberVO.java
+++ src/main/java/member/model/MemberVO.java
@@ -1,5 +1,31 @@
 package member.model;
 
 public class MemberVO {
+	
+	private int no;
+	private String name;
+	private String email;
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
